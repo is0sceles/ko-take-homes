32e9ac2a29f56634847f9028110b828cd484fc13
Wasiff <wasiff@gmail.com>
2017-09-26 19:56:01 -0700

    add search feature

--- front-end/movies-evan-likes/src/components/Movies.js
+++ front-end/movies-evan-likes/src/components/Movies.js
@@ -8,28 +8,44 @@ export default class Movies extends React.Component {
     this.state = {
       movies: Data.movies,
       reviews: Data.reviews,
+      search: '',
     };
   }
+  filterSearch(e) {
+    this.setState({
+      search: e.target.value,
+    });
+  }
   render() {
+    const filteredMovies = this.state.movies
+          .filter(movie => movie.title.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1);
     return (
       <div>
         <div className="filter-list">
           <form>
             <fieldset className="form-group">
-              <input type="text" placeholder="Search" onChange={e => console.log(e.target.value)} />
+              <input
+                type="text"
+                placeholder="Search by title"
+                onChange={this.filterSearch.bind(this)}
+                value={this.state.search}
+              />
             </fieldset>
           </form>
         </div>
-        <ul>
-          {this.state.movies.sort((a, b) => {
-            const A = a.title;
-            const B = b.title;
-            return (A < B) ? -1 : (A > B) ? 1 : 0;
-          }).map(movie => <li key={movie.id}>
-            {movie.score * 100}%
-          <a href={movie.url}>{movie.title}</a> {movie.year}
-          </li>)}
-        </ul>
+        <div className="movie-list">
+          <ul>
+            {filteredMovies.sort((a, b) => {
+              const A = a.title;
+              const B = b.title;
+              return (A < B) ? -1 : (A > B) ? 1 : 0;
+            }).map(movie =>
+              <li key={movie.id}>
+                {movie.score * 100}%
+                <a href={movie.url}>{movie.title}</a> ({movie.year})
+              </li>)}
+          </ul>
+        </div>
       </div>
     );
   }
